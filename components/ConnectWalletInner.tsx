37a3bc1370b95b88fd2db0fa48f46769d6a2e0cb
latest63 <Lateefolalekan18@yahoo.com>
2026-05-26 08:16:22 +0800

    style: increase connect button text size

--- components/ConnectWalletInner.tsx
+++ components/ConnectWalletInner.tsx
@@ -44,7 +44,7 @@ export default function ConnectWalletInner() {
   return (
     <button
       onClick={() => open()}
-      className="h-8 px-4 rounded-sm text-[13px] font-semibold bg-brand-dark hover:opacity-70 text-white transition-all"
+      className="h-8 px-4 rounded-sm text-[15px] font-semibold bg-brand-dark hover:opacity-70 text-white transition-all"
     >
       Connect
     </button>
